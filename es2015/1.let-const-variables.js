30ca429f6140ff6fd63e1d6f0368ca2b2e2b0482
Sabino Fernandez <sabinofernandezr@gmail.com>
2021-07-21 09:27:53 -0500

    fixed spaces for comments

--- es2015/1.let-const-variables.js
+++ es2015/1.let-const-variables.js
@@ -3,17 +3,17 @@ let a = 1;
 if (a === 1) {
     let a = 2;
 
-    console.log(a); //2
+    console.log(a); // 2
 }
 
-console.log(a); //1
+console.log(a); // 1
 
 const x = 1;
 
 if (x === 1) {
     const y = 2; // You cannot re-assign the value similar to let variable
 
-    console.log(y); //2
+    console.log(y); // 2
 }
 
-console.log(x); //1
+console.log(x); // 1
